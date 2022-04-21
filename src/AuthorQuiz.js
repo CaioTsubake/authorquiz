d953c35d0d67c6d462496a65af36d47af0082626
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2022-04-20 22:32:13 -0300

    Començando componente AuthorQuiz
    Reescrevendo o componente App. Ele vai ter a base da nossa aplicação

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './AuthorQuiz.css';
+import './bootstrap.min.css';
+
+function AuthorQuiz() {
+  return (
+    <div>Author Quiz</div>
+  );
+}
+
+export default AuthorQuiz;
