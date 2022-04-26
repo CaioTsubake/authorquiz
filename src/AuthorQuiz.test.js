65e461c5e00faeae2e9e53023cddcb647ee10589
Caio Villa Silva Tsubake <caiotsubake@gmail.com>
2022-04-25 23:41:40 -0300

    Primeiro teste escrito

--- src/AuthorQuiz.test.js
+++ src/AuthorQuiz.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
+import  ReactDOM from "react-dom";
 import AuthorQuiz from './AuthorQuiz';
 
-test('renders learn react link', () => {
+xtest('renders learn react link', () => {
   render(<AuthorQuiz />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+describe("Author Quiz", () =>{
+  it("renderiza sem quebrar", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<AuthorQuiz />, div);
+  })
+})
